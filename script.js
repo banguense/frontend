341a0019e044d3142b34c5d876cc2c64e791d6c2
Hector Fernandes <hectorhu17@gmail.com>
2025-01-23 12:27:28 -0300

    added a real api call

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ require.config({
 let editor;
 require(["vs/editor/editor.main"], function () {
   editor = monaco.editor.create(document.getElementById("editor"), {
-    value: "// Digite seu código em C aqui\n",
+    value: "// Digite seu código em MPI aqui\n",
     language: "c",
     theme: "vs-light",
     minimap: { enabled: false },
@@ -18,15 +18,58 @@ require(["vs/editor/editor.main"], function () {
 
 document.getElementById("submitBtn").addEventListener("click", async () => {
   const btn = document.getElementById("submitBtn");
+
+  if (btn.textContent.trim() === "Resultado") {
+    window.location.href = "/resultados";
+    return;
+  }
+
   const spinner = document.getElementById("spinner");
+  const numberOfWorkers = document.getElementById("numberOfWorkers").value;
+  // const numberOfProcess = document.getElementById("numberOfProcess").value;
 
   try {
     btn.disabled = true;
     spinner.classList.remove("hidden");
 
-    // Simular envio
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const code = editor.getValue();
+
+    const data = {
+      numberOfWorkers: parseInt(numberOfWorkers),
+      numberOfProcess: parseInt(numberOfWorkers),
+      code: code,
+    };
+    const response = await fetch("/api/run", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) throw new Error("Erro na execução");
+
+    const result = await response.json();
+
+    const storedResults = JSON.parse(localStorage.getItem("results") || "[]");
+    storedResults.push({
+      id: result.id,
+      output: result.output,
+      timestamp: new Date().toISOString(),
+    });
+    localStorage.setItem("results", JSON.stringify(storedResults));
+
+    if (localStorage.length > 10) {
+      localStorage.shift();
+    }
+
+    btn.textContent = "Resultado";
+    btn.classList.remove("bg-blue-500", "hover:bg-blue-600");
+    btn.classList.add("bg-green-500", "hover:bg-green-600");
   } catch (error) {
+    btn.textContent = "Enviar novamente";
+    btn.classList.remove("bg-blue-500", "hover:bg-blue-600");
+    btn.classList.add("bg-red-500", "hover:bg-red-600");
     alert("Erro ao enviar código");
   } finally {
     btn.disabled = false;
