e147af79e5c2c453b3898dff765a2f21fc02933a
Banguense <bckp.hctr@gmail.com>
2025-01-25 18:16:21 +0000

    fix: toggle now works

--- pages/resultados/script.js
+++ pages/resultados/script.js
@@ -34,7 +34,7 @@ function getResults() {
       <h2 class="text-xl text-gray-900 cursor-pointer" onclick="toggleContent('${result.id}')">
         Execução ${formattedDate}
       </h2>
-      <div id="content-${result.id}" class="${index === 0 ? "" : "hidden"}">
+      <div id="content-${result.id}" class="hidden">
         <div class="text-gray-700 space-y-2">
           <p><strong>ID:</strong> ${result.id}</p>
           <p><strong>Data:</strong> ${formattedDate}</p>
@@ -47,6 +47,7 @@ function getResults() {
     `;
     resultsContainer.appendChild(divResult);
   });
+  toggleContent(results.at(0).id);
 }
 
 function toggleContent(id) {
