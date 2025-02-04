532085da40006919246a2dfeb20299f5bc49206b
Hector Fernandes <hectorhu17@gmail.com>
2025-02-04 20:25:15 -0300

    add more infos

--- pages/resultados/script.js
+++ pages/resultados/script.js
@@ -38,6 +38,8 @@ function getResults() {
         <div class="text-gray-700 space-y-2">
           <p><strong>ID:</strong> ${result.id}</p>
           <p><strong>Data:</strong> ${formattedDate}</p>
+          <p><strong>Número de Contêineres:</strong> ${result.numberOfWorkers}</p>
+          <p><strong>Tempo de Execução:</strong> ${result.elapsedTime}</p>
           <pre class="bg-gray-50 p-4 rounded max-h-64 overflow-auto whitespace-pre-wrap break-words">${result.output}</pre>
         </div>
         <button class="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onclick="downloadResult('${result.id}')">
